79f57eb9c47449cef2123adc27dba0ab97116a11
siponse <fahrulakbar16@gmail.com>
2024-03-21 07:51:09 +0800

    fix: seeder and readmi.md

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -18,5 +20,33 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+        
+        User::create([
+            'name' => 'Super Administrator',
+            'email' => 'superadmin@gmail.com',
+            'password' => bcrypt('secretservice'),
+            'role' => User::ROLE_SUPERADMIN,
+        ]);
+
+        User::create([
+            'name' => 'Sales',
+            'email' => 'sales@gmail.com',
+            'password' => bcrypt('secretservice'),
+            'role' => User::ROLE_SALES,
+        ]);
+
+        User::create([
+            'name' => 'Admin Purchase',
+            'email' => 'admin_purchase@gmail.com',
+            'password' => bcrypt('secretservice'),
+            'role' => User::ROLE_ADMIN_PURCHASE,
+        ]);
+
+        User::create([
+            'name' => 'Admin Warehouse',
+            'email' => 'admin_warehouse@gmail.com',
+            'password' => bcrypt('secretservice'),
+            'role' => User::ROLE_ADMIN_WAREHOUSE,
+        ]);
     }
 }
